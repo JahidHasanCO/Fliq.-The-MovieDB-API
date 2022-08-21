5794a9ebc1f0268377786339a15f5c1a7ea2f43a
Jahid hasan <Vdjsovaj@gmail.com>
2022-08-21 22:52:45 +0600

    update

--- routes/movie.py
+++ routes/movie.py
@@ -2,19 +2,26 @@ from fastapi import APIRouter
 
 from models.movie import Movie
 from config.db import db
-from schemas.movie import serializeDict , serializeList
+from schemas.movie import serializeDict, serializeList
 from bson import ObjectId
 
 appMovieRouter = APIRouter()
+
+
+@appMovieRouter.get('/')
+async def index():
+    return "Hello World"
 
 
 @appMovieRouter.get('/movie')
 async def movies():
     return serializeList(db.movies.find())
 
+
 @appMovieRouter.get('/movies/{id}')
 async def movie_details(id):
-    return serializeDict(db.movies.find_one({"_id":ObjectId(id)}))
+    return serializeDict(db.movies.find_one({"_id": ObjectId(id)}))
+
 
 @appMovieRouter.post('/movie')
 async def movie(movie: Movie):
@@ -26,10 +33,10 @@ async def movie(movie: Movie):
 
 
 @appMovieRouter.put('/movie/{id}')
-async def update_movie(id,movie: Movie):
-    db.movies.find_one_and_update({"_id":ObjectId(id)},
-        {"$set": dict(movie)}
-    )
+async def update_movie(id, movie: Movie):
+    db.movies.find_one_and_update({"_id": ObjectId(id)},
+                                  {"$set": dict(movie)}
+                                  )
     return {
         "msg": "Successfully movie Updated.",
     }
@@ -37,7 +44,7 @@ async def update_movie(id,movie: Movie):
 
 @appMovieRouter.delete('/movie/{id}')
 async def delete_movie(id):
-    db.movies.find_one_and_delete({"_id":ObjectId(id)})
+    db.movies.find_one_and_delete({"_id": ObjectId(id)})
     return {
         "msg": "Successfully Deleted.",
     }
